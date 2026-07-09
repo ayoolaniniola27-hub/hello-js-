9fc6b8ac721c607a623072be35163b036dcad7c6
Mubashir Ayoola <ayoolaniniola27@gmail.com>
2026-07-09 12:24:15 +0100

    Add personal.js

---
+++ Intro.js
@@ -0,0 +1,3 @@
+console.log("Name: Mubashir Ayoola");
+console.log("Age: 20"); 
+console.log("Favourite Programming Language: JavaScript");
---
+++ Personal.js
@@ -0,0 +1,6 @@
+console.log("Name: Levis AY");
+console.log("Age: 25");
+console.log("School: ASCI");
+console.log("Place of work: Ijegun");
+console.log("Position: Office Attendant");
+console.log("Ater That: Nextposition");
